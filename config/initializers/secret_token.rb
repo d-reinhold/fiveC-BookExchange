1422eff305b019eecdef2c98143c8318bcf77272
Dominick Reinhold <dominick.reinhold@yahoo.com>
2011-12-09 21:53:11 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FiveCBookExchange::Application.config.secret_token = '820d4df1f35bd4b656de0dd5f6b78b8a591a1273f273bb738df745fe092a59a99408fd0c084980b67f9988f386d1a03a2586c29074d1f342edb461f9afd6208e'
