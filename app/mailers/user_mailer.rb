b0788623c873d7e7b0ee37d691fcfe7e4543cdf6
Dominick Reinhold <dominick.reinhold@yahoo.com>
2011-12-28 00:18:59 -0600

    made sure the from address was configured correctly

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,9 @@ class UserMailer < ActionMailer::Base
     @user = user
     @url  = "http://fivecbookexchange.herokuapp.com"
     email_with_name = "#{@user.name} <#{@user.email}>"
-    mail(:to => email_with_name, :subject => "Welcome to the 5C Book Exchange!")
+    #mail(:to => email_with_name, :subject => "Welcome to the 5C Book Exchange!")
+    mail(:to => 'check-auth2@verifier.port25.com', :subject => "Welcome to the 5C Book Exchange!")
+    
   end
   
   
