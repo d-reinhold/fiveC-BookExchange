be977b76498a4290286b80250ecb0c245251d2ab
Dominick Reinhold <dominick.reinhold@yahoo.com>
2012-01-07 09:25:49 -0600

    added listing mailer to send notifications when listings are created

---
+++ app/mailers/listing_mailer.rb
@@ -0,0 +1,13 @@
+class ListingMailer < ActionMailer::Base
+  default from: "5C Book Exchange <fivecbookexchange@sendgrid.me>"
+  
+  def listed_book(listing)
+    @listing = listing
+    @user = listing.user
+    @url  = "http://fivecbookexchange.herokuapp.com"
+    email_with_name = "#{@user.name} <#{@user.email}>"
+    mail(:to => email_with_name, :subject => "Listing created on the 5C Book Exchange!")
+    #mail(:to => 'check-auth2@verifier.port25.com', :subject => "Welcome to the 5C Book Exchange!")
+  end
+  
+end
