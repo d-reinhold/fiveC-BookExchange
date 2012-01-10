45f369f3d001144e7b758c4e63765ebd488c3740
Dominick Reinhold <dominick.reinhold@yahoo.com>
2012-01-09 18:42:12 -0600

    added staging alert to emails

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -2,7 +2,7 @@
 
 FiveCBookExchange::Application.configure do
   config.middleware.insert_after(::Rack::Lock, "::Rack::Auth::Basic", "Staging") do |u, p|
-    [u, p] == ENV['STAGING_USERNAME'], ENV['STAGING_SECRET']]
+    [u, p] == [ENV['STAGING_USERNAME'], ENV['STAGING_SECRET']]
     
   end
   
