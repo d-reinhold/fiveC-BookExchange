ebe8d4d3edcf307ed36e43dea91bf04f2c1b4755
Dominick Reinhold <dominick.reinhold@yahoo.com>
2012-01-11 09:39:37 -0600

    allowed @pomona.edu to sign up for testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ActiveRecord::Base
 
   def is_fivec_email
     #if email.include?("@pomona.edu")
-      errors.add(:email, "Please use '@mymail.pomona.edu'") 
+    #  errors.add(:email, "Please use '@mymail.pomona.edu'") 
     #else 
       unless (email.include?("@students.pitzer.edu")) or (email.include?("@mymail.pomona.edu")) or email.include?("@pomona.edu") or (email.include?("@scrippscollege.edu")) or (email.include?("@hmc.edu"))  or (email.include?("@g.hmc.edu")) or (email.include?("@pitzer.edu")) or (email.include?("@cmc.edu"))
         errors.add(:email, "is not a valid 5C email address")
