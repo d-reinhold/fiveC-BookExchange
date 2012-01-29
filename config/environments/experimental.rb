fbae37259ee490bf1ac80f17ecd22b3e040c26b9
Dominick Reinhold <dominick.reinhold@yahoo.com>
2012-01-28 17:36:57 -0800

    getting ready to hide site

---
+++ config/environments/experimental.rb
@@ -0,0 +1,22 @@
+# config/environments/staging.rb
+
+FiveCBookExchange::Application.configure do
+  config.middleware.insert_after(::Rack::Lock, "::Rack::Auth::Basic", "Experimental") do |u, p|
+    [u, p] == [ENV['EXPERIMENTAL_USERNAME'], ENV['EXPERIMENTAL_SECRET']]
+    
+  end
+  
+  
+  # Set up the mailer with SendGrid
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com'
+  }
+  ActionMailer::Base.delivery_method = :smtp
+
+  #... other config
+end
