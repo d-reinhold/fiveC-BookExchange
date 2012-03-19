7c20c51e1922e7cf6220e25491b2edf5b24184fb
Dominick Reinhold <dominick.reinhold@yahoo.com>
2012-03-19 16:07:54 -0700

    added new buttons/layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,24 @@
 
 
 $(document).ready(function() {
-	
+	window.fbAsyncInit = function() {
+    FB.init({
+      appId      : 274758929266415,
+    	channelUrl : 'http://localhost:3000',
+      status     : true, 
+      cookie     : true,
+      xfbml      : true,
+      oauth      : true,
+    });
+  };
+  (function(d){
+     var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+     js = d.createElement('script'); js.id = id; js.async = true;
+     js.src = "//connect.facebook.net/en_US/all.js";
+     d.getElementsByTagName('head')[0].appendChild(js);
+   }(document));
+   
+   
 	$("a#inline").fancybox({
 		'modal': true
 	});
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '274758929266415', 'b740a6e6a52c11d9477d061c79f430dd',
-  :scope => 'email,user_education_history, offline_access', :display => 'popup'
+           :scope => 'email,user_education_history, offline_access', 
+           :display => 'popup'
 end
