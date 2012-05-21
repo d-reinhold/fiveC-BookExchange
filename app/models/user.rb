a2a60c8bc2a753a72be9a1ef8053e71328f42295
Dominick Reinhold <dominick.reinhold@yahoo.com>
2012-05-21 16:52:28 -0700

    fixed bug when users had no fb education history

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,29 +22,34 @@ class User < ActiveRecord::Base
   def update_with_omniauth(auth)
     self.name = auth["info"]["name"]
     self.email = auth["info"]["email"]
-    schools = auth["extra"]["raw_info"]["education"].each.select{|s| s["type"] == 'College'}
-    @claremont_colleges = School.find_by_name('Claremont Colleges')
-    claremont_colleges = ['Pomona College', 'Claremont McKenna College', 
-                          'Harvey Mudd College', 'Scripps College',
-                          'Pitzer College', 'Keck Graduate Institute',
-                          'Claremont Graduate University', 'Claremont Colleges']
-    schools.each do |s| 
-      @school = School.where("name = ?", s["school"]["name"]).limit(1).first
-      if @school.nil? # if no one from this school has signed up before
-        @school = School.create(:name => s["school"]["name"])
-        self.schools << @school
-      end
-      unless self.schools.all.include?(@school)
-        puts "Adding #{@school.name} to your schools."
-        self.schools << @school
-      end
-      if claremont_colleges.include?(@school.name)
-        self.default_school_name = @claremont_colleges.name
-        self.default_school_id = @claremont_colleges.id   
-        unless self.schools.all.include?(@claremont_colleges)
-          self.schools << @claremont_colleges
+    education = auth["extra"]["raw_info"]["education"]
+    unless education.nil?
+      schools = education.each.select{|s| s["type"] == 'College'}
+      @claremont_colleges = School.find_by_name('Claremont Colleges')
+      claremont_colleges = ['Pomona College', 'Claremont McKenna College', 
+                            'Harvey Mudd College', 'Scripps College',
+                            'Pitzer College', 'Keck Graduate Institute',
+                            'Claremont Graduate University', 'Claremont Colleges']
+      unless schools.nil?
+        schools.each do |s| 
+          @school = School.where("name = ?", s["school"]["name"]).limit(1).first
+          if @school.nil? # if no one from this school has signed up before
+            @school = School.create(:name => s["school"]["name"])
+            self.schools << @school
+          end
+          unless self.schools.all.include?(@school)
+            puts "Adding #{@school.name} to your schools."
+            self.schools << @school
+          end
+          if claremont_colleges.include?(@school.name)
+            self.default_school_name = @claremont_colleges.name
+            self.default_school_id = @claremont_colleges.id   
+            unless self.schools.all.include?(@claremont_colleges)
+              self.schools << @claremont_colleges
+            end
+            puts "Setting #{self.default_school_name} to be your current network."       
+          end
         end
-        puts "Setting #{self.default_school_name} to be your current network."       
       end
     end
     if self.default_school_id.nil?
